bbbd8ce68538853227ecd1cf78b21799f8ab04ae
Fiona Lapham <fionadlapham@gmail.com>
2018-02-28 17:25:48 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,9 +6,8 @@ class Song
 end
 
   def artist_name= (name_of_artist)
-    artist= Artist.find_or_create_by_name(name_of_artist)
-    @artist= artist
-    @artist.add_song(self)
+    artist_name= Artist.find_or_create_by_name(name_of_artist)
+    artist_name.add_song(self)
   end
 
 
@@ -17,7 +16,7 @@ end
     name_of_artist = split_file[0]
     song_name = split_file[1]
     new_song = self.new(song_name)
-    new_song.artist_name= name_of_artist
+    new_song.artist_name = name_of_artist
     new_song
   end
 
